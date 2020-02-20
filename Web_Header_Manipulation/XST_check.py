172682a856c7cd1d7239c072d7d746609f630b47
h000698 <hieu-nguyen@cybozu.vn>
2020-02-20 13:23:34 +0700

    dd

--- Web_Header_Manipulation/XST_check.py
+++ Web_Header_Manipulation/XST_check.py
@@ -3,13 +3,13 @@ import requests
 # line creates an array of the HTTP methods we are going to send
 verbs = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'TRACE', 'TEST']
 for verb in verbs:
-    req = requests.request(verb, 'http://10.224.152.109/cgi-bin/cbgrn/grn.cgi')
+    req = requests.request(verb, 'http://10.224.152.109/')
     # It prints out the method and the response status code and reason:
     print(verb, req.status_code, req.reason)
 
     # There is a section of code to specifically test for XST
-    
+
     if verb == 'TRACE' and 'TRACE / HTTP/1.1' in req.text:
-        print('Possible Cross Site Tracing vulnerability found')
+            print('Possible Cross Site Tracing vulnerability found')
 
 
